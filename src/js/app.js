0d65d9020d431d279f91471a4a0665b41444fcb9
Jase Rader <jaserader@me.com>
2016-09-18 16:34:25 -0500

    Initial Commit

---
+++ src/js/app.js
@@ -0,0 +1,39 @@
+import $ from 'jquery';
+
+var jsonpData;
+var clinic;
+var template;
+
+$.get( 'template/template.html', function(data){
+  template = data;
+});
+
+var processTemplate = function() {
+  var rendered = Mustache.render( template, clinic );
+  $( '#template-output' ).append( rendered );
+};
+
+$.ajax({
+  url: 'https://api.inquickerstaging.com/v2/schedules?api_key=c874c82cea1d3cdd29e0e609b9c601909cf818f7&facilities=womens-health-of-tennessee',
+  type: 'GET',
+  dataType: 'jsonp',
+  success: function(data){
+    if(data){
+      jsonpData = data.data;
+      setupData();
+    }
+  },
+  error: function(e) {
+  }
+});
+
+function setupData(){
+
+  jsonpData.forEach(
+    function(arrayItem){
+      clinic = arrayItem;
+      processTemplate();
+      //sets up the template for each object.
+    }
+  );
+}
